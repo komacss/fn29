5b43103a78b897e699b3324402c9eb343da7837a
Kama <komacss11@gmail.com>
2026-09-20 16:22:30 +0500

    juft son funksiyasi

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-print("Salom dunyo")
+def juft(sonlar):
+    natija = []
+    for i in sonlar:
+        if i % 2 == 0:
+            natija.append(i)
+    return natija
 
-def summa(a, b):
-    return a + b
-
-def multiplay(a, b):
-    return a * b
+sonlar = [12, 7, 5, 18, 21, 30, 44, 9]
+print(juft(sonlar))       

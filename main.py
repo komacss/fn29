d342aa9b5c02374c18663d5f117c9594c8d11fa3
Kama <komacss11@gmail.com>
2026-09-20 16:39:56 +0500

    kichik va katta son

--- main.py
+++ main.py
@@ -7,3 +7,19 @@ def juft(sonlar):
 
 sonlar = [12, 7, 5, 18, 21, 30, 44, 9]
 print(juft(sonlar))       
+
+
+def min_max(sonlar):
+    eng_katta = eng_kichik = sonlar[0]
+    for son in sonlar:
+        if son > eng_katta: eng_katta = son
+        if son < eng_kichik: eng_kichik = son
+    print(eng_katta, eng_kichik)
+
+
+sonlar = []
+matn = input("Sonlarni kiriting: ")
+for son in matn.split():
+    sonlar.append(int(son))
+
+min_max(sonlar)
